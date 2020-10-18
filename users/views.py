2a2964b85c6edd29d2c0163225b096f75093e0bd
Cullen D <cullen.dolan@gmail.com>
2020-10-18 11:47:05 -0500

    added styling for username

--- users/views.py
+++ users/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # so users can sign up for a site
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save() # saves user and keeps it secure
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
             return redirect('blog-home') # redirects to url pattern
+
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 '''
