7a4e229053a308b3d7c9905a347462b916a9fd64
Cullen D <cullen.dolan@gmail.com>
2020-10-18 11:18:33 -0500

    adding the meeage to confirm new user

--- users/views.py
+++ users/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # so users can sign up for a site
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('blog-home') # redirects to url pattern
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+'''
+message methods:
+messages.debug
+messages.info
+messages.success
+messages.warning
+messages.error
+'''
