c1b617df7de93012f8bba0c0dd51c0ce7790b789
Cullen D <cullen.dolan@gmail.com>
2020-10-20 22:21:34 -0500

    added links to posts from home page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # function view to create home page
 def home(request):
@@ -11,12 +11,16 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
-# list view
+# list view for home page
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts' #setting attribtue equal to posts in home function
     ordering = ['-date_posted']
+
+# list view
+class PostDetailView(DetailView):
+    model = Post
 
 
 # function view to create about page
