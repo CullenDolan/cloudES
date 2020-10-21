aee0f40a1e5bd150262229f1c29ff86a0f705004
Cullen D <cullen.dolan@gmail.com>
2020-10-20 22:10:12 -0500

    chenged ordering of blog posts and changing the type of view for the home page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView
 
-
+# function view to create home page
 def home(request):
     context = {
         'posts':Post.objects.all
@@ -10,7 +11,18 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
+# list view
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts' #setting attribtue equal to posts in home function
+    ordering = ['-date_posted']
+
+
+# function view to create about page
 def about(request):
     return render(request, 'blog/about.html', {'title':"About"})
+
+# class view to display update and delete posts
 
 
