19f83f911e55f30d24015d8c724c52dd4ef56751
Cullen D <cullen.dolan@gmail.com>
2020-10-22 21:53:49 -0500

    add links to get posts by specific users

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 # function view to create home page
 def home(request):
@@ -18,8 +19,20 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts' #setting attribtue equal to posts in home function
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 5
 
+
+# list view for specific user
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_post.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts' #setting attribtue equal to posts in home function
+    paginate_by = 5
+
+    #get the user
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 # list view for reading specific blogs
 class PostDetailView(DetailView):
