10fd74a9527d372414ebcdf2a8b969df5ac4ae50
Cullen D <cullen.dolan@gmail.com>
2020-10-22 20:20:51 -0500

    changes to only allow users to update their own blog posts

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # function view to create home page
 def home(request):
@@ -28,10 +28,18 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content'] # add a field for pictures
 
- # list view  for updating blogs
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+ # list view  for updating blogs and only if ithe user created the blog
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content'] # add a field for pictures
+
+    # check current user is author
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
     # autmoatically makes home is signed in as the submitter
     def form_valid(self, form):
